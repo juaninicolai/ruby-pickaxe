4a5519c7f0f0dea823d64ee858ede870445c2a28
juaninicolai <juan.nicolai@maersk.com>
2026-01-04 17:27:41 +0100

    add ruby accessor methods and attributes

--- classes.rb
+++ classes.rb
@@ -1,7 +1,34 @@
 class BookInStock
+# 2 another way to declare accessor methods
+  attr_reader :isbn
+  attr_accessor :price
+
   def initialize(isbn, price)
     @isbn = isbn
     @price = Float(price)
+  end
+
+# 1 accessor methods
+=begin
+  def isbn
+    @isbn
+  end
+
+  def price
+    @price
+  end
+# setter methods end with = sign
+  def price=(new_price)
+    @price = new_price
+  end
+=end
+
+  def price_in_cents
+    (price * 100).round
+  end
+
+  def price_in_cents=(cents)
+    @price = cents / 100.0
   end
 
   def to_s
@@ -17,3 +44,11 @@ p b2
 
 b3 = BookInStock.new("isbn3", "5.67")
 p b3
+
+book = BookInStock.new("isbn1", 33.80)
+puts "ISBN  = #{book.isbn}"
+puts "Price = #{book.price}"
+puts "Price = #{book.price_in_cents}"
+book.price_in_cents = 1234
+puts "Price = #{book.price}"
+puts "Price in cents = #{book.price_in_cents}"
